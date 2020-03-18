886f0578314fdb6700176c2024b15d919abb0c7f
Bastien <bzg@gnu.org>
2020-03-18 17:36:19 +0100

    Protect down/upload from throwing an error

--- src/covid19/upload_datagouv.clj
+++ src/covid19/upload_datagouv.clj
@@ -8,6 +8,7 @@
 
 (def datagouv-api-token (System/getenv "DATAGOUV_API_TOKEN"))
 
+;; https://www.data.gouv.fr/fr/admin/dataset/5e723be64ea6945002c092dc
 (def datagouv-covid19-dataset "5e723be64ea6945002c092dc")
 
 (def datagouv-endpoint-format
@@ -24,7 +25,7 @@
 
 (def datagouv-covid19-resources
   {"file1.csv"
-  {:resource "26db01a9-891c-4b41-be85-d597b949a6c0" }})
+   {:resource "26db01a9-891c-4b41-be85-d597b949a6c0" }})
 
 (defn get-filename-from-url [url]
   (s/replace url #"^.+/([^/]+)$" "$1"))
@@ -32,18 +33,24 @@
 (defn download-spf-files [files]
   (println "Downloading SPF files...")
   (doseq [f files]
-    (let [fn (get-filename-from-url f)]
-      (curl/get f {:raw-args ["--output" fn] })))
+    (let [f-n (get-filename-from-url f)]
+      (try (curl/get f {:raw-args ["--output" f-n] })
+           (catch Exception _
+             (println "Error while downloading" f-n)))))
   (println "Downloading SPF files... done"))
 
 (defn upload-spf-files [files]
   (println "Uploading SPF files...")
   (doseq [f files]
-    (let [resource (get-in datagouv-covid19-resources [f :resource])]
-      (curl/post
-       (format datagouv-endpoint-format resource)
-       (merge datagouv-api-headers
-              {:form-params {"file" (str "@" f)}}))))
+    (let [f-n      (get-filename-from-url f)
+          resource (get-in datagouv-covid19-resources
+                           [f-n :resource])]
+      (try (curl/post
+            (format datagouv-endpoint-format resource)
+            (merge datagouv-api-headers
+                   {:form-params {"file" (str "@" f-n)}}))
+           (catch Exception _
+             (println "Error while uploading" f-n)))))
   (println "Uploading SPF files... done"))
 
 (defn -main []
